bbeb210e05f42973448d7ea44a051596a08f5544
ihllwizard <temitope.idowu27@gmail.com>
2020-01-25 13:34:39 +0100

    this kind commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           RandomGenerator generator = new RandomGenerator();    
+        int rand = generator.RandomNumber(5, 100);    
+        Console.WriteLine($"Random number between 5 and 100 is {rand}");    
+    
+        string str = generator.RandomString(10, false);    
+        Console.WriteLine($"Random string of 10 chars is {str}");    
+    
+        string pass = generator.RandomPassword();    
+        Console.WriteLine($"Random string of 6 chars is {pass}");    
+    
+        Console.ReadKey();
+        }
+    }
+}
